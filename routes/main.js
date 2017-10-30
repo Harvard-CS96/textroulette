06ce99ba1e9414c2b6186958de4e13288432245c
William Bryk <williambryk@college.harvard.edu>
2017-10-30 04:18:23 -0400

    Correct code, cleaned up updatePreferences

--- routes/main.js
+++ routes/main.js
@@ -6,6 +6,18 @@ var questions = require(path.join(DIR.ROOT, 'controllers/questions'));
 
 const router = express.Router();
 
+
+var bodyParser = require('body-parser')
+router.use( bodyParser.json() );       // to support JSON-encoded bodies
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+router.use(express.json());       // to support JSON-encoded bodies
+router.use(express.urlencoded());
+
+
+
+
 router.get('/', (req, res) => {
     res.sendFile(path.join(DIR.PUBLIC, "index.html"))
 })
@@ -13,6 +25,15 @@ router.get('/', (req, res) => {
 router.get('/questions/', questions.findForUser);
 
 router.get('/questions', questions.findAll);
+
+
+router.get('/updatePreferences', (req, res) => {
+    res.sendfile(path.join(DIR.PUBLIC, "updatePreferences.html"))
+})
+
+router.post('/updatePreferences', (req, res) => {
+    res.send(req.body)
+})
 
 module.exports = router;
 
