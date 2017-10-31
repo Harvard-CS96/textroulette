f209b218d2fde3bfc3c88e581f7bbb5f290fcf03
Russell Pekala <russellpekala@college.harvard.edu>
2017-10-30 22:34:12 -0400

    checkpoint

--- routes/main.js
+++ routes/main.js
@@ -27,13 +27,48 @@ router.get('/questions/', questions.findForUser);
 router.get('/questions', questions.findAll);
 
 
-router.get('/updatePreferences', (req, res) => {
-    res.sendfile(path.join(DIR.PUBLIC, "updatePreferences.html"))
+router.get('/updatePreferences', isLoggedIn, (req, res) => {
+	console.log(req.user);
+	console.log('update Prefs called');
+    res.sendFile(path.join(DIR.PUBLIC, "updatePreferences.html"))
 })
 
 router.post('/updatePreferences', (req, res) => {
     res.send(req.body)
 })
+
+router.get('/login', (req, res) => {
+    res.render("login", {
+        fb_id: process.env.FB_ID
+    })
+})
+
+// route for facebook authentication and login
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+    // handle the callback after facebook has authenticated the user
+router.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+        successRedirect : '/updatePreferences',
+        failureRedirect : '/'
+    }))
+
+    // route for logging out
+router.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    })
+
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
 
 module.exports = router;
 
