dbfca04bdb662f18dae3bbb8245e1489419c9efa
Jacob Lurye <jlurye@college.harvard.edu>
2017-10-30 23:22:31 -0400

    Updated User model.

--- db/models/user.js
+++ db/models/user.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    // Define user schema here
-})
+    uuid: String,
+    facebook: {
+        id   : Number,
+        name : String,
+        token: String
+    },
+    date_registered: {type: Date, default: Date.now}
+}, { collection: 'users' })
 
 module.exports = mongoose.model('User', UserSchema);
