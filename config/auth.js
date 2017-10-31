1fc1e3d18b6b311bb5d4a20149af5010f0d0aa7e
Russell Pekala <russellpekala@college.harvard.edu>
2017-10-30 22:34:12 -0400

    checkpoint

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+// This file holds authentication details.  
+// If we add google/twitter auth later that code will go here
+
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : 'your-secret-clientID-here', // your App ID
+        'clientSecret'  : 'your-client-secret-here', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    }
+
+};
