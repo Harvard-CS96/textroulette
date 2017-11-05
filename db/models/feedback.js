c26ce904da1ddd216bf1ab8d5dd73dac104ff39a
Jacob Lurye <jlurye@college.harvard.edu>
2017-11-05 11:53:15 -0500

    Update models to include badges.

--- db/models/feedback.js
+++ db/models/feedback.js
@@ -4,11 +4,15 @@ const uuid = require('uuid');
 require('mongoose-uuid2')(mongoose);
 var UUID = mongoose.Types.UUID;
 
+const BadgeSchema = require('./badge');
+
 const FeedbackSchema = new mongoose.Schema({
     giver: UUID,
     receiver: UUID,
     chat: String,
     stars: Number,
-    badges: [],
+    badges: { type: [BadgeSchema], default: [] },
     text: String
-}, { collection: 'feedback'});
+}, { collection: 'feedback' });
+
+module.exports = mongoose.model("Feedback", FeedbackSchema);
