4aea61c8f127be1f9da095371681c0f2f366afbd
William Bryk <williambryk@college.harvard.edu>
2017-11-13 17:36:06 -0500

    Made a queue version of Matcher.js, have not test it yet with multiple users

--- matcher.js
+++ matcher.js
@@ -19,6 +19,7 @@ class Matcher {
         this.callbacks = {};
         this._setStatus = setStatus;
         this._maxBlacklist = maxBlacklist;
+        this.connectionsQueue = []
     }
 
     // Add a callback that fires when we switch to a certain status
@@ -168,7 +169,7 @@ class Matcher {
                                 });
 
         // Iterate over all connections
-        const ids = Object.keys(this.connections)
+        const ids = this.connectionsQueue;
         const { length } = ids;
         for (let i = 0; i < length; i++) {
             const key = ids[i]
@@ -193,14 +194,17 @@ class Matcher {
                         if (isDifferentOpinion(question1.response, Questions2[Question2Index].response)) {
                             // set partner on conversation about question with this id
                             var questionTitle = getQuestionByID(question1.id, questionData);
-                            this.setPartner(id, key, {text: questionTitle, id: question1.id});
-                            break;
+
+                            // if you can find a partner stop searching
+                            if (this.setPartner(id, key, {text: questionTitle, id: question1.id}))
+                                break;
                         }
                     }
                 }
             }
         }
-
+        // if the loop hasn't broken then put id on the queue
+        this.connectionsQueue.push(id);
     }
         
 
@@ -212,7 +216,7 @@ class Matcher {
             this.connections[id2].partner !== null
         ) {
             // TODO: handle if you can't pair
-            return;
+            return false;
         }
         console.log(`Matcher: Pairing ${id1} and ${id2}`)
 
@@ -220,6 +224,14 @@ class Matcher {
         this.connections[id1].partner = id2;
         this.connections[id2].partner = id1;
 
+        // delete from queue
+        if (this.connectionsQueue.indexOf(id1) > -1) {
+            this.connectionsQueue.splice(id1);
+        }
+        if (this.connectionsQueue.indexOf(id2) > -1) {
+            this.connectionsQueue.splice(id2);
+        }
+        
         console.log("Users will discuss the question: " + question.text);
 
         this._setStatus(id1, PAIRING, id2);
@@ -228,6 +240,7 @@ class Matcher {
             uid1: this.connections[id1].user_id, 
             uid2: this.connections[id2].user_id
         });
+        return true;
     }
 
     // Get the partner of a given id
