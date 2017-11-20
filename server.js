e72708ff166cbd8fc42665bfe0bc5109fd2524a2
Nicholas Boucher <nickboucher32@gmail.com>
2017-11-20 17:39:21 -0500

    Removed https

--- server.js
+++ server.js
@@ -37,21 +37,10 @@ var multer = require('multer');
 var config = require('getconfig'),
     fs = require('fs'),
     sockets = require('./sockets'),
-    port = parseInt(process.env.PORT || config.server.port, 10),
-    https = require('https'),
     express = require('express');
 
-// Create an http(s) server instance to that socket.io can listen to
-var options = {
-    key: fs.readFileSync(config.server.key),
-    cert: fs.readFileSync(config.server.cert),
-};
-
 var app = express();
-
-var server = https.createServer(options, app).listen(port, function() {
-    console.log("Express server listening on port " + port);
-});
+var server = require("http").createServer(app);
 
 var io = require("socket.io").listen(server);
 
